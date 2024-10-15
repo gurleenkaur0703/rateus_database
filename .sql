bdd7093bce2d999e26ecffff845a14cc0ebfb6c0
gurleenkaur0703 <gurleenkaur.gk03@gmail.com>
2024-10-15 14:13:17 +0530

    Update .sql

--- .sql
+++ .sql
@@ -3,17 +3,17 @@ CREATE DATABASE RateUs;
 USE RateUs;
 
 CREATE TABLE Organisation(
-    Org_ID varchar(10) PRIMARY KEY,
-    Org_Name varchar(200),
+    ID varchar(10) PRIMARY KEY,
+    Name varchar(200),
     Owner_id varchar(10),
     Logo varchar(100),
-    Org_Address varchar(200),
+    Address varchar(200),
     Listed_On DATE DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE User_(
-    User_ID varchar(10) PRIMARY KEY,
-    User_Name varchar(100),
+    ID varchar(10) PRIMARY KEY,
+    Name varchar(100),
     Email varchar(200),
     Contact varchar(12),
     Password varchar(300),
@@ -21,7 +21,7 @@ CREATE TABLE User_(
 );
 
 CREATE TABLE Items(
-    Items_ID varchar(10) PRIMARY KEY,
+    ID varchar(10) PRIMARY KEY,
     Item_Name varchar(100),
     Org_ID varchar(10),
     Added_On DATE DEFAULT CURRENT_TIMESTAMP
@@ -31,21 +31,21 @@ CREATE TABLE Reviews (
     Review_ID VARCHAR(10) PRIMARY KEY,
     Org_ID VARCHAR(10),
     Item_ID VARCHAR(10),
-    Review VARCHAR(100)
+    Review VARCHAR(1000)
 );
 
 CREATE TABLE Ratings (
     Rating_ID VARCHAR(10) PRIMARY KEY,
     Item_ID VARCHAR(10),
-    Rating VARCHAR(10)
+    Rating DECIMAL(3,1) CHECK (Rating >= 1 AND Rating <= 10)
 );
 
 CREATE TABLE Aggregate_Ratings (
     Item_ID VARCHAR(10) PRIMARY KEY,
-    Rating VARCHAR(10)
+    Rating DECIMAL(3,1) CHECK (Rating >= 1 AND Rating <= 10)
 );
 
 CREATE TABLE Sentimental_Scores (
     Review_ID VARCHAR(10) PRIMARY KEY,
-    Sentimental_Score VARCHAR(100)
+    Sentimental_Score INT CHECK (Sentimental_Score IN (0, 1))
 );
